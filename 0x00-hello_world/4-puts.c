4832ba7c5119273ac451c1b30075b50b6a2293d3
iamabba <iamabbamusa@gmail.com>
2022-09-10 08:24:18 -0700

    4 put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,18 @@
 
 
 /**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+*
+* * main - print the string in the put function
+*
+* *
+*
+* * Description: using the main function
+*
+* * this program prints "Programming is like building a multilingual puzzle
+*
+* * Return: 0
+*
+* */
 
 int main(void)
 
