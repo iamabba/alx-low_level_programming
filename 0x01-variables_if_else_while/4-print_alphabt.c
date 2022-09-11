0c0ff0bc10ccd7a796f6a4b2b4826b54eb7b6c4a
iamabba <iamabbamusa@gmail.com>
2022-09-11 03:23:42 -0700

    4 print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alpha a-z but remove 'q' and 'e'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			if ((alpha != 'q') && (alpha != 'e'))
+			{
+				putchar(alpha);
+			}
+			alpha++;
+		}
+		putchar('\n')
+
+		return (0);
+}
+
