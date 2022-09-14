46699cbb332cc074a6fde6a65ce1bd141c669258
iamabba <iamabbamusa@gmail.com>
2022-09-13 18:43:16 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,8 +15,9 @@ for (m = 0; m < 60; m++)
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
 _putchar(':');
-_putchar((m / 10) + '0');
-putchar((m % 10) + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
+}
+
