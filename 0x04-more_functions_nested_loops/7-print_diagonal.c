c9a91e27ee5514ded727b3876acc354f875ebe9f
iamabba <iamabbamusa@gmail.com>
2022-09-16 20:06:58 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,34 +1,25 @@
 #include "main.h"
 
 /**
- * print_diagonal - print character "\" n times
- * @n: number of characters to draw
+ * print_square - print a square of #'s given size
+ * @size: size to draw
  * Return: 0
  */
 
-void print_diagonal(int n)
+void print_square(int size)
 {
-int draw;
-int space;
-
-if (n > 0)
+int row, column;
+if (size > 0)
 {
-for (draw = 1; draw <= n; draw++)
+for (row = 1; row <= size; row++)
 {
-for (space = 1; space < draw; space++)
-_putchar(' ');
-_putchar('\\');
+for (column = 1; column <= size; column++)
+{
+_putchar('#');
+}
 _putchar('\n');
 }
 }
 else
 _putchar('\n');
 }
-Footer
-
-© 2022 GitHub, Inc.
-
-Footer navigation
-
-Terms
-
